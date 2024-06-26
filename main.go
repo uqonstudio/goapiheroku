adcd258529d40c74503a70534badafa3ccd8ebdb
uqonstudio <sdesain25@gmail.com>
2024-06-26 12:25:21 +0700

    Add get users function

--- main.go
+++ main.go
@@ -49,10 +49,67 @@ func main() {
 		})
 	})
 
+	router.GET("/users", GetEmployee)
+
 	// Start the server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	router.Run(":" + port)
+}
+
+type Employee struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phoneNumber"`
+	Address     string `json:"address"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+	Department  string `json:"department"`
+}
+
+func GetEmployee(c *gin.Context) {
+	var employees []Employee
+	name := c.Query("name")
+	var rows *sql.Rows
+	var err error
+
+	query := "SELECT id, name, email, address, phoneNumber, department FROM ms_employee"
+	if name == "" {
+		rows, err = db.Query(query)
+	} else {
+		query += " WHERE name = $1;"
+		rows, err = db.Query(query, name)
+	}
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var emp Employee
+		err = rows.Scan(&emp.Id, &emp.Name, &emp.Email, &emp.Address, &emp.PhoneNumber, &emp.Department)
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		employees = append(employees, emp)
+	}
+	if err = rows.Err(); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	if len(employees) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "employee.name tidak ditemukan"})
+		return
+	} else {
+		c.JSON(200, gin.H{
+			"message": "data employee",
+			"data":    employees,
+		})
+	}
 }
